5acac3aad2421c5a411dd4a912d4b9decbd27d3c
IamBITUSgg <88578601+IamBITUSgg@users.noreply.github.com>
2021-08-30 14:00:30 +0300

    Add files via upload

---
+++ build/precache-manifest.50f0fea3ae48861476f65a123c6b75df.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f1ec7d7dc1d83c3743c",
+    "url": "./static/css/main.990236aa.chunk.css"
+  },
+  {
+    "revision": "8f1ec7d7dc1d83c3743c",
+    "url": "./static/js/main.adfdb9d8.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "5ae753fa94e4f8a5a085",
+    "url": "./static/css/2.4f2e0ee2.chunk.css"
+  },
+  {
+    "revision": "5ae753fa94e4f8a5a085",
+    "url": "./static/js/2.7e7f0cf7.chunk.js"
+  },
+  {
+    "revision": "c53e482c55280e18695f533a6fe47bee",
+    "url": "./static/media/coin.c53e482c.png"
+  },
+  {
+    "revision": "1808b5f6b2dd330757f13c6dc3ddf32a",
+    "url": "./static/media/1233.1808b5f6.ttf"
+  },
+  {
+    "revision": "d8dde2c45fabb1c7700b35203f77be7a",
+    "url": "./static/media/click.d8dde2c4.png"
+  },
+  {
+    "revision": "bc1dee2b5a8abf0b1eb6e63952664401",
+    "url": "./index.html"
+  }
+];
